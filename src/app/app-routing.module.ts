26bc8fb1b895d15caf5e2655029a2f7ccce88e94
Jesus Ivan Villarreal Hernandez <desarrollohg01@hgtransportaciones.com>
2023-12-04 17:07:03 -0600

    Ajustes en correccion de busqueda por empresa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
 const routes: Routes = [
-  { path: 'navbar', component: NavbarComponent },
   { path: 'body', component: BodyComponent },
   { path: 'footer', component: FooterComponent },
   { path: '', redirectTo: '/body', pathMatch: 'full' },
